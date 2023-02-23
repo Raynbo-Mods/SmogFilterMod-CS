26258d674bf5dd561ba85f3f9a9e78334c86d6ec
kye <kye-a@hotmail.com>
2023-02-24 01:38:11 +1100

    Minor fixes
    Removed unnecessary usings (Code cleanup)
    Moved to nuget package (saves getting dlls and makes managing a bit easier)
    Removed serialized attributes from recipes as recipes should not be serialized

--- Filter.cs
+++ Filter.cs
@@ -1,34 +1,12 @@
-﻿namespace Eco.Mods.TechTree
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using Eco.Gameplay.Blocks;
-    using Eco.Gameplay.Components;
-    using Eco.Gameplay.DynamicValues;
-    using Eco.Gameplay.Items;
-    using Eco.Gameplay.Objects;
-    using Eco.Gameplay.Players;
-    using Eco.Gameplay.Skills;
-    using Eco.Gameplay.Systems;
-    using Eco.Gameplay.Systems.TextLinks;
-    using Eco.Shared.Localization;
-    using Eco.Shared.Serialization;
-    using Eco.Shared.Utils;
-    using Eco.Core.Items;
-    using Eco.World;
-    using Eco.World.Blocks;
-    using Eco.Gameplay.Pipes;
-    using Eco.Gameplay.GameActions;
-    using System.Runtime.Serialization;
-    using Eco.Gameplay.Pipes.LiquidComponents;
-    using System.IO.Pipes;
-    using Eco.Shared.Math;
-    using Eco.WebServer.Web.Controllers;
-    using Eco.Core.Controller;
-    using Eco.Gameplay.Components.Auth;
-    using Eco.Gameplay.Disasters;
+﻿using Eco.Gameplay.Components;
+using Eco.Gameplay.Items;
+using Eco.Gameplay.Skills;
+using Eco.Shared.Localization;
+using Eco.Shared.Serialization;
+using Eco.Core.Items;
 
+namespace Eco.Mods.TechTree
+{
     [Serialized]
     [LocDisplayName("Filter")]
     [Weight(100)]
@@ -39,7 +17,8 @@
     {
         public override LocString DisplayDescription => Localizer.DoStr("An item used to filter impurities from gasses. NOTE: some particals are to small to remove");
     }
-    [Serialized]
+
+
     [RequiresSkill(typeof(MechanicsSkill), 3)]
     public partial class FilterRecipe : RecipeFamily
     {
